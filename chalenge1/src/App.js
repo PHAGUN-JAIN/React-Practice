3ec9031e1ad50bfe45cfc7511c42610af9c0dc87
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-25 18:45:25 +0530

    src files

---
+++ chalenge1/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Hello React!
+      </header>
+      <p>
+        this is a sample para
+      </p>
+      <ol>
+        <li>
+           item1
+        </li>
+        <li>
+           item2
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
